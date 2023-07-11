c2cee992e048d25ff9507a1cc7824258b36a43e5
yurimoyses09 <yuri09.moyses@gmail.com>
2023-07-10 22:04:06 -0300

    Primeiro commit

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+	private FilmeContext _context;
+	private IMapper _mapper;
+
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+	public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
+	{
+		Filme filme = _mapper.Map<Filme>(filmeDto);
+
+		_context.Filmes.Add(filme);
+		_context.SaveChanges();
+
+		return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+	}
+
+	[HttpGet]
+	public IActionResult RecuperaFilmes([FromQuery]int skip = 0, [FromQuery]int take = 50)
+	{
+		var filmes = _context.Filmes.Skip(skip).Take(take);
+
+		return Ok(new { data = filmes.ToList() });
+	}
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+		var filme = _context.Filmes.Where(x => x.Id == id).FirstOrDefault();
+		if (filme == null)
+			return NotFound();
+
+        return Ok(new { data = filme });
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O titulo do filme é obrigatorio")]
+    public string Titulo { get; set; }
+    
+    [Required(ErrorMessage = "O genero do filme é obrigatorio")]
+    [MaxLength(50, ErrorMessage = "O tamanho do genero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    
+    [Required(ErrorMessage = "O titulo do filme é obrigatorio")]
+    [Range(70, 600, ErrorMessage = "A duração dever ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
